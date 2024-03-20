46ced5d645d25a5ae190d946be13c4c3f4b1faba
iqbalya25 <iqbalya25@gmail.com>
2024-03-20 15:11:19 +0700

    adding new sout

--- src/Newfile.java
+++ src/Newfile.java
@@ -1,5 +1,9 @@
 public class Newfile {
     public static void main(String[] args) {
         System.out.println("Try and Error");
+        System.out.println("Try and Error");
+        System.out.println("Try and Error");
+        System.out.println("Try and Error");
+        System.out.println("Try and Error");
     }
 }
